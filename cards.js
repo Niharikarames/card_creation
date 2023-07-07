e7f178d0cfd2737dff02c06ad4c4dfcb406371fc
Niharika R <niharika.r@corp.knowledgelens.com>
2023-07-07 18:26:25 +0530

    Initial commit

---
+++ cards.js
@@ -0,0 +1,13 @@
+function openModal(title, info) {
+      var modal = document.getElementById("modal");
+      var modalTitle = document.getElementById("modal-title");
+      var modalInfo = document.getElementById("modal-info");
+
+      modalTitle.textContent = title;
+      modalInfo.textContent = "Information: " + info;
+      modal.classList.add("active");
+    }
+
+    function closeModal() {
+      var modal = document.getElementById("modal");
+      modal.classList.remove("active");
